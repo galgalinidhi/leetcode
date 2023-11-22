0607d47befc83f5b4733096d29db8e5e9975376b
Nidhi Galgali <44816413+galgalinidhi@users.noreply.github.com>
2023-11-22 03:31:09 -0500

    Time: 208 ms, Memory: 17.2 MB - LeetHub

--- number-of-provinces/number-of-provinces.py
+++ number-of-provinces/number-of-provinces.py
@@ -1,42 +1,58 @@
-class UnionFind(object):
-
-    def __init__(self, parents: list, rank: list):
-        self.parents = parents
-        self.rank = rank
-
-    def find(self, node):
-
-        parent = node 
-        while parent != self.parents[parent]:
-            
-            self.parents[parent] = self.parents[self.parents[parent]]
-            parent = self.parents[parent]
-        return parent
+# class Solution:
+#     def findCircleNum(self, isConnected: List[List[int]]) -> int:    
     
-    def union(self, node1, node2):
-        first, second = self.find(node1), self.find(node2)
-        if first == second: return 0 
-
-        if self.rank[first] > self.rank[second]:
-            self.parents[second] = first
-            self.rank[first] += self.rank[second]
-        else:
-            self.parents[first] = second
-            self.rank[second] += self.rank[first]
-        return 1 
-
-class Solution:
-
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-
-        parents = [node for node in range(len(isConnected))]
-        rank = [1] * len(parents)
-
-        unionFind = UnionFind(parents=parents, rank=rank)
-        result = len(parents) 
+#         parent = [0]* len(isConnected[0])
+#         for i in range(0, len(isConnected)):
+#             parent[i] = i
         
-        for edge1 in range(len(isConnected)):
-            for otherEdge in range(len(isConnected)):
-                if isConnected[edge1][otherEdge] == 1:
-                    result -= unionFind.union(edge1, otherEdge)
-        return result
+#         #no path compression
+#         def find(x):
+#             while x != parent[x]:
+#                 x = parent[x]
+#             return x
+    
+#         def union (x,y):
+#             x_val = find(x)
+#             y_val = find(y)
+        
+#             if x_val!=y_val:
+#                 parent[y] = x_val
+        
+        
+#         for i in range(0,len(isConnected)):
+#             for j in range(0,len(isConnected)):
+#                 if i == j:
+#                     continue
+                    
+#                 if isConnected[i][j] == 1:
+#                     union(i,j)
+                
+#         return len(set(parent))        
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        
+        l1 = [0]*len(isConnected[0])
+        for i in range(len(l1)):
+            l1[i] = i
+        
+        
+        def get_root(x):
+            return l1[x]
+        
+      
+        def union(x,y):
+            Rx = get_root(x)
+            Ry = get_root(y)
+
+            if(Rx != Ry):
+                for i in range(0,len(l1)):
+                    if l1[i] == Ry:
+                        l1[i] = Rx
+
+        
+        for i in range(0,len(isConnected)):
+            for j in range(i+1,len(isConnected[0])):
+                if isConnected[i][j] == 1:
+                    union(i,j)
+            
+        return len(set(l1))
