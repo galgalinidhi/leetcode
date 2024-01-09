96c68055c327f8f2d899bf92dec30d213e92c580
Nidhi Galgali <44816413+galgalinidhi@users.noreply.github.com>
2024-01-09 13:49:19 -0800

    Time: 27 ms (98.73%), Space: 17.5 MB (8.04%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -21,10 +21,8 @@ class Solution:
             return res 
       
         r1 = dfs(root1)
-        # print(r1)
         res = []
         r2 = dfs(root2)
-        # print(r2)
         
         if r1 == r2:
             return True
